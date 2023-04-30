2a4b2f3818ec249664ca2a80505cc6b38fcbefa7
Oliver Killane <oliverkillane@gmail.com>
2023-04-30 23:34:58 +0200

    60029: Added Vector operations

--- 60029 - Data Processing Systems/advanced_topics/code/vectorisation.cpp
+++ 60029 - Data Processing Systems/advanced_topics/code/vectorisation.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 
 template<size_t n>
+__attribute__((noinline))
 void element_sum_scalar(int64_t x[n], int64_t y[n], int64_t z[n]) {
     for (auto i = 0; i < n; i++) {
         z[i] = x[i] + y[i];
@@ -15,24 +16,34 @@ void element_sum_scalar(int64_t x[n], int64_t y[n], int64_t z[n]) {
 }
 
 template<size_t n>
+__attribute__((noinline))
 typename std::enable_if<n % 8 == 0, void>::type element_sum_vec(int64_t x[n], int64_t y[n], int64_t z[n]) { 
-    const size_t vs = n / 8;
-    
-    __m512i* x_vecs = reinterpret_cast<__m512i*>(x);
-    __m512i* y_vecs = reinterpret_cast<__m512i*>(y);
-    __m512i* z_vecs = reinterpret_cast<__m512i*>(z);
-
-    for (auto i = 0; i < vs; i++) {
-        z_vecs[i] = _mm512_add_epi64(x_vecs[i], y_vecs[i]);
+    for (auto i = 0; i < n; i+=8) {
+        __m512i xs = _mm512_loadu_si512(&x[i]);
+        __m512i ys = _mm512_loadu_si512(&y[i]);
+        __m512i zs = _mm512_add_epi64(xs, ys);
+        _mm512_storeu_si512(&z[i], zs);
     }
 }
 
-int main() {
-    int64_t x[2048] = {0};
-    int64_t y[2048] = {0};
-    int64_t z[2048] = {0};
+template<size_t n>
+void randomarray(int64_t arr[n]) {
+    for (auto i = 0; i < n; i++) arr[i] = std::rand();
+}
 
-    element_sum_vec<2048>(x, y, z);
+template<size_t n>
+void demo() {
+    int64_t x[n];
+    int64_t y[n];
+    int64_t z[n];
+
+    randomarray<n>(x); randomarray<n>(y);
+
+    element_sum_vec<n>(x, y, z);
 
     for (auto e : z) std::cout << e << ", ";
+}
+
+int main() {
+    demo<2048>();
 }
